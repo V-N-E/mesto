f9c564454bce18dccb041dddc76696d6d610b291
Vladislav Nekrasov <nekrasoffv2020@yandex.ru>
2021-01-26 13:49:17 +0300

    внесены поправки после ревью

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let popup = document.querySelector('.popup');
-let popupOpen = document.querySelector('.profile__editButton');
-let popupClose = document.querySelector('.popup__closeButton');
-let popupSaveAndClose = document.querySelector('.popup__saveButton');
+let popupOpen = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close-button');
+let popupSaveAndClose = document.querySelector('.popup__save-button');
 
 popupOpen.onclick = function() {
   popup.style.display="block";
@@ -17,14 +17,14 @@ popupSaveAndClose.onclick = function() {
 
 let formElement = document.querySelector('.popup__form');
 
-let nameInput = document.querySelector('.popup__nameInput');
-let descriptionInput = document.querySelector('.popup__descriptionInput');
+let nameInput = document.querySelector('.popup__name-input');
+let descriptionInput = document.querySelector('.popup__description-input');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let nameValue = document.querySelector('.popup__nameInput').value;
-    let descriptionValue = document.querySelector('.popup__descriptionInput').value;
+    let nameValue = document.querySelector('.popup__name-input').value;
+    let descriptionValue = document.querySelector('.popup__description-input').value;
 
     let name = document.querySelector('.profile__name');
     let description = document.querySelector('.profile__description');
